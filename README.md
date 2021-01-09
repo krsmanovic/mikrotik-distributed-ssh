d66d50546cd02b34da53a5b361b79c25962aec1a
krsmanovic <cedo.krsmanovic@gmail.com>
2021-01-09 11:27:15 +0100

    Updated script for Python version 3

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-## Python 2.7 MikroTik SSH script (based on "paramiko" module)
+# MikroTik SSH script based on Python "paramiko" module
 
-This is a tool for executing many MikroTik commands from external `commands` file to many MikroTik routers listed in `hosts` file. 
+This is a tool for executing many MikroTik commands from external `commands` file to many MikroTik routers listed in `hosts` file. Use this type of script only if you are not able to setup ssh keys for whatever the reason. Remember to remove all credentials left in clear text after you have finished your work!
 
-Log file `error.log` is also created, since it is very important to know if something went wrong when batch configuring many devices.
+Log files `error.log` and `success.log` are created since it is very important to know if something went wrong when batch configuring many devices.
 
-Run the script by issuing
+## 1) Setup the inventory and commands
+
+Populate the files `hosts` (inventory) and `commands` according to your task. Smaple files are provided in `/scripts` folder together with the source code.
+
+## 2) Enter the credentials within the script
+
+Edit the mt_username and mt_password variables to fit your credentials.
+
+It is strongly advised never to keep credentials in clear text and use ssh keys where possible! Remember to remove credentials after you are finished with your work!
+
+## 3) Run the script
+
+Simply start the script with
 ```
 python mikrotik-ssh.py
 ```
-
-Example files are included in `/script` folder together with Python source.
--- script/mikrotik-ssh.py
+++ script/mikrotik-ssh.py
@@ -10,74 +10,77 @@ try:
     
 except ImportError:
     sys.tracebacklimit=0
-    with open("error.log","ab") as e:
-        e.write(time.strftime("%Y.%m.%d") + " " + time.strftime("%H:%M:%S") + " \"Paramiko\" module missing! Please visit http://www.paramiko.org/installing.html for more details." + "\r\n")
+    with open("error.log","a") as e:
+        e.write(time.strftime("%Y-%m-%d") + "_" + time.strftime("%H:%M:%S") + " \"Paramiko\" module missing! Please visit http://www.paramiko.org/installing.html for more details." + "\n")
     e.close()
     raise ImportError("\rPlease install \"paramiko\" module! Visit http://www.paramiko.org/installing.html for more details.\r\n")
 
 nlines = 0
 f = open("hosts","r")
-mt_username = "admin_username"
-mt_password = "admin_password"
+mt_username = "script_username"
+mt_password = "script_password"
 timeout = 10
 
 for line in f:
     
     if nlines > 0:
-        print "\r\nWaiting 3s before logging on to the next device...\n"
+        print("\r\nWaiting 3s before logging on to the next device...\n")
         time.sleep(3)
     
     nlines += 1
-    conn_date = time.strftime("%Y.%m.%d")
+    conn_date = time.strftime("%Y-%m-%d")
     conn_time = time.strftime("%H:%M:%S")
     host = line.rstrip("\n")
     ssh = paramiko.SSHClient()
     
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    print "\r\nConnecting to " + str(nlines) + ". host: " + host + "\r\n"
+    print("\r\nConnecting to " + str(nlines) + ". host: " + host + "\r\n")
 
     try: 
         ssh.connect(host,username=mt_username,password=mt_password,timeout=timeout)
         
     except socket.timeout as e:
-        print "\r\nConnection timeout. Log entry created."
-        with open("error.log","ab") as e:
-            e.write(conn_date + " " + conn_time + " " + mt_username + "@" + host + " Timeout connecting to the device." + "\r\n")
+        print("\r\nConnection timeout. Log entry created.")
+        with open("error.log","a") as e:
+            e.write(conn_date + "_" + conn_time + " " + mt_username + "@" + host + " Timeout connecting to the device." + "\r\n")
         e.close()
         continue
 
     except paramiko.AuthenticationException:
-        print "\r\nWrong credentials. Log entry created."
-        with open("error.log","ab") as e:
-            e.write(conn_date + " " + conn_time + " " + mt_username + "@" + host + " Wrong credentials." + "\r\n")
+        print("\r\nWrong credentials. Log entry created.")
+        with open("error.log","a") as e:
+            e.write(conn_date + "_" + conn_time + " " + mt_username + "@" + host + " Wrong credentials." + "\r\n")
         e.close()
         continue
 
     except:
-        print "\r\nError connecting to the device. Log entry created."
-        with open("error.log","ab") as e:
-            e.write(conn_date + " " + conn_time + " "  + mt_username + "@" + host + " Unknown error while connecting to the device." + "\r\n")
+        print("\r\nError connecting to the device. Log entry created.")
+        with open("error.log","a") as e:
+            e.write(conn_date + "_" + conn_time + " "  + mt_username + "@" + host + " Unknown error while connecting to the device." + "\r\n")
         e.close()
         continue
 
-    print "\r\nSuccsessfully connected to the host. Executing commands from the external file:\r\n"
+    print("\r\nSuccsessfully connected to the host. Executing commands from the external file:\r\n")
     
     k = open("commands","r")
     for line in k:
         mt_command = line.rstrip("\n")
         time.sleep(.3)
         stdin, stdout, stderr = ssh.exec_command(mt_command)
-        print mt_command
+        print(mt_command)
 
-    print "\nExternal commands are executed successfully.\n"
+    print("\nExternal commands are executed successfully.\n")
+    with open("success.log","a") as s:
+        s.write(conn_date + "_" + conn_time + " Successfully executed commands on the host " + host + "\n")
+    s.close()
     k.close()
     ssh.get_transport().close()
     ssh.close()
         
 if nlines == 0:
-    print "\nList of hosts is empty.\n"
+    print("\nList of hosts is empty.\n")
 else:
-    print "\nEnd of the program.\n"
+    print("\nEnd of the program.\n")
 f.close()
 quit()
