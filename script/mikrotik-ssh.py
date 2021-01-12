12fcac06ccc2f1e126736257409d764198a0357b
krsmanovic <cedo.krsmanovic@gmail.com>
2021-01-12 21:33:57 +0100

    Cleaned up the output

--- script/mikrotik-ssh.py
+++ script/mikrotik-ssh.py
@@ -5,19 +5,29 @@ import errno
 import sys
 import time
 
+def time_stamp():
+    t = time.strftime("%Y-%m-%d %H:%M:%S")
+    return t
+
 try:
     import paramiko
     
 except ImportError:
     sys.tracebacklimit=0
     with open("error.log","a") as e:
-        e.write(time.strftime("%Y-%m-%d") + "_" + time.strftime("%H:%M:%S") + " \"Paramiko\" module missing! Please visit http://www.paramiko.org/installing.html for more details." + "\n")
+        e.write(time_stamp() + " \"Paramiko\" module missing! Please visit http://www.paramiko.org/installing.html for more details.\n")
     e.close()
     raise ImportError("\rPlease install \"paramiko\" module! Visit http://www.paramiko.org/installing.html for more details.\r\n")
 
+try:
+    f = open("hosts","r")
+except IOError:
+    sys.tracebacklimit=0
+    print("\nFile \"hosts\" does not exist or is not accessible.\n")
+    quit()
+
 nlines = 0
-f = open("hosts","r")
-mt_username = "script_username"
+mt_username = "script_user"
 ssh_key = paramiko.RSAKey.from_private_key_file("key.ppk")
 # Using ssh keys is strongly advised!
 # If you are unable to setup that method of connecting to your devices, you have the option of the clear text login as well.
@@ -25,20 +35,21 @@ ssh_key = paramiko.RSAKey.from_private_key_file("key.ppk")
 timeout = 10
 
 for line in f:
-    
-    if nlines > 0:
-        print("\r\nWaiting 3s before logging on to the next device...\n")
-        time.sleep(3)
+
+    try:
+        k = open("commands","r")
+    except IOError:
+        sys.tracebacklimit=0
+        print("\nFile \"commands\" does not exist or is not accessible.\n")
+        quit()
     
     nlines += 1
-    conn_date = time.strftime("%Y-%m-%d")
-    conn_time = time.strftime("%H:%M:%S")
     host = line.rstrip("\n")
     ssh = paramiko.SSHClient()
     
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    print("\r\nConnecting to " + str(nlines) + ". host: " + host + "\r\n")
+    print("\r\n########################################## Connecting to " + str(nlines) + ". host: " + host + " ##########################################\r\n")
 
     try: 
         ssh.connect(host,username=mt_username,pkey=ssh_key,timeout=timeout)
@@ -46,38 +57,38 @@ for line in f:
         # ssh.connect(host,username=mt_username,password=mt_password,timeout=timeout)
         
     except socket.timeout as e:
-        print("\r\nConnection timeout. Log entry created.")
+        print("Connection timeout. Log entry created.")
         with open("error.log","a") as e:
-            e.write(conn_date + "_" + conn_time + " " + mt_username + "@" + host + " Timeout connecting to the device." + "\r\n")
+            e.write(time_stamp() + " " + host + " Timeout connecting to the device.\n")
         e.close()
         continue
 
     except paramiko.AuthenticationException:
-        print("\r\nWrong credentials. Log entry created.")
+        print("Wrong credentials. Log entry created.")
         with open("error.log","a") as e:
-            e.write(conn_date + "_" + conn_time + " " + mt_username + "@" + host + " Wrong credentials." + "\r\n")
+            e.write(time_stamp() + " " + host + " Wrong credentials.\n")
         e.close()
         continue
 
     except:
-        print("\r\nError connecting to the device. Log entry created.")
+        print("Error connecting to the device. Log entry created.")
         with open("error.log","a") as e:
-            e.write(conn_date + "_" + conn_time + " "  + mt_username + "@" + host + " Unknown error while connecting to the device." + "\r\n")
+            e.write(time_stamp() + " " + host + " Unknown error while connecting to the device.\n")
         e.close()
         continue
 
-    print("\r\nSuccsessfully connected to the host. Executing commands from the external file:\r\n")
-    
-    k = open("commands","r")
+    print("Succsessfully connected to the host. Executing commands from the external file:\r\n")
+
     for line in k:
         mt_command = line.rstrip("\n")
-        time.sleep(.3)
+        # Adding 200ms delay between commands
+        time.sleep(.2)
         stdin, stdout, stderr = ssh.exec_command(mt_command)
         print(mt_command)
 
-    print("\nExternal commands are executed successfully.\n")
+    print("\nExternal commands are executed successfully.")
     with open("success.log","a") as s:
-        s.write(conn_date + "_" + conn_time + " Successfully executed commands on the host " + host + "\n")
+        s.write(time_stamp() + " " + host + " Successfully executed commands on the host.\n")
     s.close()
     k.close()
     ssh.get_transport().close()
@@ -86,6 +97,6 @@ for line in f:
 if nlines == 0:
     print("\nList of hosts is empty.\n")
 else:
-    print("\nEnd of the program.\n")
+    print()
 f.close()
 quit()
